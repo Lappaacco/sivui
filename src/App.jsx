cd732bcbf4a7fa32a861af90a6337f4c6b3b4506
Lappaacco <teemupaunonen@gmail.com>
2026-02-14 13:57:55 +0200

    Aseta suuret kiinteät korkeudet Vellolle: 1200/1300/1400px

--- src/App.jsx
+++ src/App.jsx
@@ -869,8 +869,8 @@ export default function App() {
                 🎁 {t('bookingSection.giftCardInfo')}
               </p>
 
-              {/* Vello embed container - min height but can grow */}
-              <div className="w-full min-h-[800px] md:min-h-[900px] lg:min-h-[1000px] relative">
+              {/* Vello embed container - large fixed height to show full calendar view */}
+              <div className="w-full h-[1200px] md:h-[1300px] lg:h-[1400px] relative">
                 {velloLoading && (
                   <div className="absolute inset-0 flex items-center justify-center bg-white/70 z-20">
                     <div className="loader" aria-hidden></div>
